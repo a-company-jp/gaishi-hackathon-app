71e037cdd6ca767677814f94c227f03abc695857
pirotyyy <hiroki1423@docomo.ne.jp>
2024-11-14 23:55:43 +0900

    :sparkles: ページ下部に固定

--- webapp/src/components/layout/Footer.tsx
+++ webapp/src/components/layout/Footer.tsx
@@ -3,17 +3,19 @@ import { DollarSign, Menu } from "lucide-react";
 
 function Footer() {
   return (
-    <footer className="grid grid-cols-2">
-      <button className="flex flex-col items-center">
-        <Menu className="h-6 w-6" />
-        <span className="text-xs mt-1">メニュー</span>
-      </button>
-      <button className="items-center">
-        <div className="flex flex-col items-center">
-          <DollarSign className="h-6 w-6" />
-          <span className="text-xs mt-1">お会計</span>
-        </div>
-      </button>
+    <footer className="absolute bottom-0 w-screen">
+      <div className="grid grid-cols-2">
+        <button className="flex flex-col items-center">
+          <Menu className="h-6 w-6" />
+          <span className="text-xs mt-1">メニュー</span>
+        </button>
+        <button className="items-center">
+          <div className="flex flex-col items-center">
+            <DollarSign className="h-6 w-6" />
+            <span className="text-xs mt-1">お会計</span>
+          </div>
+        </button>
+      </div>
     </footer>
   );
 }
