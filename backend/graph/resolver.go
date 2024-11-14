38f739cfad468b54ccf94a6aa96f93753d88b4bd
Shion Ichikawa <shion1305@gmail.com>
2024-11-15 00:38:23 +0900

    🔥 remove non-existent references

--- backend/graph/resolver.go
+++ backend/graph/resolver.go
@@ -1,19 +1,17 @@
 package graph
 
-import "github.com/a-company-jp/gaishi-hackathon-app/backend/service"
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	UserSvc service.User
+	//UserSvc service.User
 }
 
 func NewResolver(
-	userService service.User,
+// userService service.User,
 ) *Resolver {
 	return &Resolver{
-		UserSvc: userService,
+		//UserSvc: userService,
 	}
 }
