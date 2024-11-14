bff0af00b42e46668ebd5dd80ba48d6d24a9c8b6
pirotyyy <hiroki1423@docomo.ne.jp>
2024-11-15 01:45:26 +0900

    :sparkles: use prepared value

--- webapp/src/components/layout/Footer.tsx
+++ webapp/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { DollarSign, Menu } from "lucide-react";
 
 function Footer() {
   return (
-    <footer className="h-[66px] absolute bottom-0 w-screen border-t">
+    <footer className="h-16 w-screen border-t">
       <div className="h-full grid grid-cols-2">
         <button className="items-center">
           <div className="flex flex-col items-center">
