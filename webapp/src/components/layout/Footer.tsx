0c93baca04474469bee36f9b0cd5c5729c2b01af
pirotyyy <hiroki1423@docomo.ne.jp>
2024-11-14 23:48:56 +0900

    :sparkles: Footerを作った

---
+++ webapp/src/components/layout/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DollarSign, Menu } from "lucide-react";
+
+function Footer() {
+  return (
+    <footer className="grid grid-cols-2">
+      <button className="flex flex-col items-center">
+        <Menu className="h-6 w-6" />
+        <span className="text-xs mt-1">メニュー</span>
+      </button>
+      <button className="items-center">
+        <div className="flex flex-col items-center">
+          <DollarSign className="h-6 w-6" />
+          <span className="text-xs mt-1">お会計</span>
+        </div>
+      </button>
+    </footer>
+  );
+}
+
+export default Footer;
