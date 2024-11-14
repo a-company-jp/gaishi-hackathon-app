b8d702350f768dfdbedc01df978658407897a035
Shion Ichikawa <shion1305@gmail.com>
2024-11-15 00:22:43 +0900

    🗃️ update migrations to create necessary tables

--- backend/db_schema/migrations/000002_create-follows.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "follows" (
-   follower_id VARCHAR(255) NOT NULL,
-   followee_id VARCHAR(255) NOT NULL,
-   followed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   PRIMARY KEY (follower_id, followee_id),
-   FOREIGN KEY (follower_id) REFERENCES "users" (id) ON DELETE CASCADE,
-   FOREIGN KEY (followee_id) REFERENCES "users" (id) ON DELETE CASCADE
-);
