1aff79de6fcf45d7821cf9f411f3f62f2ba22007
Shion Ichikawa <shion1305@gmail.com>
2024-11-16 00:13:12 +0900

    🔧 change graphql uri to adapt local environment

--- webapp/src/components/provider/ApolloProvider.tsx
+++ webapp/src/components/provider/ApolloProvider.tsx
@@ -17,7 +17,8 @@ const createClient = () => {
   // NOTE: SSR時にApolloの内部で実行されるfetch()リクエストがキャッシュを使用するため,
   // Render時にHydration errorが発生してしまうので, link.fetchOptions.cacheを"no-store"に設定する
   const httpLink = new HttpLink({
-    uri: `/api/v1/query`,
+    // add http://localhost:8080 if dev mode
+    uri: `${process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL}/api/v1/query`,
     credentials: "include",
     fetchOptions: {
       cache: "no-store",
