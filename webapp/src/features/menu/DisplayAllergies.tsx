f401acf2fd31f2852a43b3353dc99bf7b3fd5c02
pirotyyy <hiroki1423@docomo.ne.jp>
2024-11-16 00:50:09 +0900

    :sparkles: アレルギー食材の表示をする

--- webapp/src/features/menu/DisplayAllergies.tsx
+++ webapp/src/features/menu/DisplayAllergies.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 type Allergy = {
   id: string;
@@ -33,19 +41,31 @@ function DisplayAllergies({ allergies }: Props) {
   return (
     <div className="p-4 flex flex-col items-center gap-1">
       <p className="text-sm">あなたのアレルギー食材</p>
-      <div className="w-3/4 p-4 rounded-lg shadow-inner flex items-center justify-center gap-2 hover:bg-gray-400">
-        {mockAllergies.map((allergy) => {
-          return (
-            <Image
-              key={allergy.id}
-              src={`/foods/${allergy.id}.png`}
-              alt=""
-              width={24}
-              height={24}
-            />
-          );
-        })}
-      </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="w-3/4 p-4 rounded-lg shadow-inner flex items-center justify-center gap-2 hover:bg-gray-200">
+            {mockAllergies.map((allergy) => {
+              return (
+                <Image
+                  key={allergy.id}
+                  src={`/foods/${allergy.id}.png`}
+                  alt=""
+                  width={24}
+                  height={24}
+                />
+              );
+            })}
+          </div>
+        </DialogTrigger>
+        <DialogContent className="w-3/4">
+          <DialogHeader>
+            <DialogTitle>アレルギー食材リスト</DialogTitle>
+            <DialogDescription>
+              あなたのアレルギー食材について確認と編集が可能です。
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
