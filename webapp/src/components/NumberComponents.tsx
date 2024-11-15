464494e655820d192c7ef5a5d4233e2248c3cbb1
kanadome <tsuchiyakeita2025@gmail.com>
2024-11-15 23:51:37 +0900

    update: カートボタンのレイアウトを修正

--- webapp/src/components/NumberComponents.tsx
+++ webapp/src/components/NumberComponents.tsx
@@ -6,7 +6,7 @@ type NumberBadgeProps = {
 
 export function NumberBadge({ count }: NumberBadgeProps) {
   return (
-    <div className="absolute -top-0.5 -right-0.5 bg-[#FF0211] text-white text-xs font-bold rounded-full w-8 h-8 flex items-center justify-center">
+    <div className="absolute -top-0.5 -right-0.5 bg-[#FF0211] text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
       {count}
     </div>
   );
