459f0f21a1f73200ad347e343225b238214ab74b
Shion Ichikawa <shion1305@gmail.com>
2024-11-15 19:42:06 +0900

    🔧 specify static build

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Image optimization is disabled in static builds
+  },
+  distDir: "build",
 };
 
 export default nextConfig;
