6eaf56703dc507023a0728f02c5ba40f55254142
pirotyyy <hiroki1423@docomo.ne.jp>
2024-11-16 03:31:09 +0900

    :sparkles: use props

--- webapp/src/features/menu/MenuCard.tsx
+++ webapp/src/features/menu/MenuCard.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 
-function MenuCard() {
+type Props = {
+  id: string;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+function MenuCard({ id, name, price, imgUrl }: Props) {
   return (
     <div className="bg-white rounded-lg shadow">
       <div className="p-4">
         <img
-          src="/placeholder.svg"
+          src={imgUrl}
           alt=""
           className="h-36 w-full rounded-lg object-cover"
         />
         <div className="mt-2 row-span-3">
-          <p className="font-semibold">焼き鳥盛り合わせ</p>
+          <p className="font-semibold">{name}</p>
           <div className="flex items-end justify-between">
             <p>
-              ￥<span className="text-2xl font-extrabold">1,200</span>
+              ￥<span className="text-2xl font-extrabold">{price}</span>
             </p>
             <Button className="bg-yellow-400 hover:bg-yellow-500 text-gray-800">
               カートに入れる
