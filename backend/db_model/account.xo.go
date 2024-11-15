82f96b61de481deb3c57b20155fbbd3a530661b5
Shion Ichikawa <shion1305@gmail.com>
2024-11-15 23:15:15 +0900

    📦️ update xo generated code

--- backend/db_model/account.xo.go
+++ backend/db_model/account.xo.go
@@ -5,11 +5,13 @@ package db_model
 
 import (
 	"database/sql"
+
+	"github.com/google/uuid"
 )
 
 // Account represents a row from 'public.accounts'.
 type Account struct {
-	ID          int            `json:"id"`           // id
+	ID          uuid.UUID      `json:"id"`           // id
 	FirebaseUID sql.NullString `json:"firebase_uid"` // firebase_uid
 	CreatedAt   sql.NullTime   `json:"created_at"`   // created_at
 	UpdatedAt   sql.NullTime   `json:"updated_at"`   // updated_at
