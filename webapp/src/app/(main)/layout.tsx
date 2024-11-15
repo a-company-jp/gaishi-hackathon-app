a1f2bcf210e1630e180ae6e4fc36c8d8bc354419
pirotyyy <hiroki1423@docomo.ne.jp>
2024-11-15 15:49:23 +0900

    :sparkles: (main)にlayout.tsxを作成

---
+++ webapp/src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+export default function MainLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <Header />
+      <main className="py-16">{children}</main>
+      <Footer />
+    </>
+  );
+}
