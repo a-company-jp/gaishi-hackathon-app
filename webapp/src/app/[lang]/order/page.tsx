8a5466eb0b1fa1cf797fc3f3070a30c6fda029aa
rare0b <rare0b0110@gmail.com>
2024-11-16 12:16:23 +0900

    feat: orderページで言語と辞書をもつ

--- webapp/src/app/[lang]/order/page.tsx
+++ webapp/src/app/[lang]/order/page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import MenuPage from "@/features/menu/MenuPage";
+import { Language } from "@/app/types/language";
+import { getDictionary } from "../dictionaries";
 
-function Page() {
+type PageProps = {
+  params: Promise<{
+    lang: Language;
+  }>;
+};
+
+export default async function Page({ params }: Awaited<PageProps>) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang);
   return (
     <>
-      <MenuPage />
+      <MenuPage lang={lang} dict={dict} />
       {/* <MenuFooter /> */}
     </>
   );
 }
-
-export default Page;
