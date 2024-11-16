fe382664c9368c7418771d072f3e72236c6e7d7e
rare0b <rare0b0110@gmail.com>
2024-11-16 09:48:26 +0900

    feat: not-foundの場合/languageに飛ばす

--- webapp/src/app/not-found.tsx
+++ webapp/src/app/not-found.tsx
@@ -1,58 +1,14 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useMutation } from "@apollo/client";
-import { gql } from "@/gql/__generated__";
 
-const tablePath = /^\/order\/table\/.+/g;
-const menuDetailPath = /^\/order\/menu\/.+/g;
-
-const Custom404: React.FC = () => {
+export default function NotFound() {
   const router = useRouter();
-  const [isNotFound, setIsNotFound] = useState(false);
-  // get table id from url
 
-  const JOIN_TABLE_SESSION = gql(`
-          mutation JoinTableSession($tableUUID: ID!) {
-            joinTableSession(tableUUID: $tableUUID){
-              tableSession {
-                id
-              }
-            }
-          }
-        `);
-  const [joinTableSession] = useMutation(JOIN_TABLE_SESSION);
   useEffect(() => {
-    const pathName = window.location.pathname;
+    router.replace("/language");
+  }, [router]);
 
-    // `/table/[id]` のパターンにマッチするか確認
-    if (pathName.match(tablePath)) {
-      const id = pathName.split("/order/table/")[1]; // id を抽出
-      if (id) {
-        const id = window.location.pathname.split("/order/table/")[1];
-        joinTableSession({ variables: { tableUUID: id } }).then((res) => {
-          if (res.data?.joinTableSession?.tableSession?.id) {
-            router.push("/order/start");
-            return;
-          }
-          setIsNotFound(true);
-        });
-      }
-    }
-    if (pathName.match(menuDetailPath)) {
-      const id = pathName.split("/order/menu/")[1]; // id を抽出
-      if (id) {
-        // `id` を `localStorage` に保存
-        localStorage.setItem("menuId", id);
-        router.push(pathName);
-        return;
-      }
-    }
-    setIsNotFound(true);
-  }, [router, joinTableSession]);
-  if (isNotFound) return <h1>404 - Page Not Found</h1>;
   return null;
-};
-
-export default Custom404;
+}
