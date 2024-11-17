09caffdbb55b9c833dc90c9629e0bbfd82ccc0d8
Shion Ichikawa <shion1305@gmail.com>
2024-11-17 19:51:25 +0900

    🧱 remove frontend service

--- infra/envs/dev/google_cloud_run_v2.tf
+++ infra/envs/dev/google_cloud_run_v2.tf
@@ -1,75 +1,75 @@
-resource "google_cloud_run_v2_service" "frontend" {
-  annotations          = {}
-  client               = "cloud-console"
-  client_version       = null
-  custom_audiences     = []
-  deletion_protection  = false
-  description          = null
-  ingress              = "INGRESS_TRAFFIC_ALL"
-  invoker_iam_disabled = false
-  labels               = {}
-  launch_stage         = "GA"
-  location             = var.region
-  name                 = "frontend-main"
-  project              = google_project.itadakimasu.project_id
-  template {
-    annotations           = {}
-    encryption_key        = null
-    execution_environment = null
-    labels = {
-      github_sha = "test"
-    }
-    max_instance_request_concurrency = 80
-    revision                         = null
-    service_account                  = "gh-actions@itadakimasu-engulid.iam.gserviceaccount.com"
-    session_affinity                 = false
-    timeout                          = "300s"
-    containers {
-      args        = []
-      command     = []
-      depends_on  = []
-      image       = "${var.region}-docker.pkg.dev/${google_project.itadakimasu.project_id}/${google_artifact_registry_repository.itadakimasu.repository_id}/frontend:latest"
-      name        = "frontend-1"
-      working_dir = null
-      ports {
-        container_port = 3000
-        name           = "http1"
-      }
-      resources {
-        cpu_idle = true
-        limits = {
-          cpu    = "1000m"
-          memory = "512Mi"
-        }
-        startup_cpu_boost = true
-      }
-      startup_probe {
-        failure_threshold     = 1
-        initial_delay_seconds = 0
-        period_seconds        = 240
-        timeout_seconds       = 240
-        tcp_socket {
-          port = 3000
-        }
-      }
-    }
-    scaling {
-      max_instance_count = 100
-      min_instance_count = 0
-    }
-  }
-  traffic {
-    percent  = 100
-    revision = null
-    tag      = null
-    type     = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-  }
-  lifecycle {
-    ignore_changes = [
-      template[0].labels["github_sha"]
-    ]
-  }
-}
+# resource "google_cloud_run_v2_service" "frontend" {
+#   annotations          = {}
+#   client               = "cloud-console"
+#   client_version       = null
+#   custom_audiences     = []
+#   deletion_protection  = false
+#   description          = null
+#   ingress              = "INGRESS_TRAFFIC_ALL"
+#   invoker_iam_disabled = false
+#   labels               = {}
+#   launch_stage         = "GA"
+#   location             = var.region
+#   name                 = "frontend-main"
+#   project              = google_project.itadakimasu.project_id
+#   template {
+#     annotations           = {}
+#     encryption_key        = null
+#     execution_environment = null
+#     labels = {
+#       github_sha = "test"
+#     }
+#     max_instance_request_concurrency = 80
+#     revision                         = null
+#     service_account                  = "gh-actions@itadakimasu-engulid.iam.gserviceaccount.com"
+#     session_affinity                 = false
+#     timeout                          = "300s"
+#     containers {
+#       args        = []
+#       command     = []
+#       depends_on  = []
+#       image       = "${var.region}-docker.pkg.dev/${google_project.itadakimasu.project_id}/${google_artifact_registry_repository.itadakimasu.repository_id}/frontend:latest"
+#       name        = "frontend-1"
+#       working_dir = null
+#       ports {
+#         container_port = 3000
+#         name           = "http1"
+#       }
+#       resources {
+#         cpu_idle = true
+#         limits = {
+#           cpu    = "1000m"
+#           memory = "512Mi"
+#         }
+#         startup_cpu_boost = true
+#       }
+#       startup_probe {
+#         failure_threshold     = 1
+#         initial_delay_seconds = 0
+#         period_seconds        = 240
+#         timeout_seconds       = 240
+#         tcp_socket {
+#           port = 3000
+#         }
+#       }
+#     }
+#     scaling {
+#       max_instance_count = 100
+#       min_instance_count = 0
+#     }
+#   }
+#   traffic {
+#     percent  = 100
+#     revision = null
+#     tag      = null
+#     type     = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+#   }
+#   lifecycle {
+#     ignore_changes = [
+#       template[0].labels["github_sha"]
+#     ]
+#   }
+# }
 
 resource "google_cloud_run_v2_service" "backend" {
   annotations          = {}
